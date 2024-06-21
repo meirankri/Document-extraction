f67d11e993c93d83528ae0e2a3be9cdf0f5160e6
meir ankri <meirankri@gmail.com>
2024-06-21 10:02:47 +0200

    add sentry to logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+
 export const logger = ({
   message,
   context,
@@ -7,7 +9,11 @@ export const logger = ({
 }) => {
   return {
     info: () => console.log(message, context),
-    error: () => console.error(message, context),
+    error: () => {
+      Sentry.setContext("error context", { context: JSON.stringify(context) });
+      Sentry.captureException(new Error("something went wrong"));
+      process.env.NODE_ENV !== "production" && console.error(message, context);
+    },
     warn: () => console.warn(message, context),
   };
 };
